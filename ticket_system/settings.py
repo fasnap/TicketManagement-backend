445f4becbe0ba02644cc57f1d7cd48ed307d7270
fasnap <fasnapkdy@gmail.com>
2025-02-08 02:23:54 +0530

    added backend url in allowed host

--- ticket_system/settings.py
+++ ticket_system/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://ticket-management-frontend-three.vercel.app/']
+ALLOWED_HOSTS = ['https://ticket-management-frontend-three.vercel.app/', 'ticketmanagement-backend.onrender.com']
 
 
 # Application definition
